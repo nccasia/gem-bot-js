b2aced797224a820293f9776c6e64c92ccf6b3bd
vinh-nguyenquoc <105034812+vinh-nguyenquoc@users.noreply.github.com>
2022-05-26 17:15:23 +0700

    Update GemSwapInfo.js

--- scripts/GemSwapInfo.js
+++ scripts/GemSwapInfo.js
@@ -1,13 +1,16 @@
 class GemSwapInfo {
-    constructor(index1, index2, sizeMatch, type)
+    constructor(index,index1, index2, sizeMatch, type, modifier)
     {
+        this.index  = index;
         this.index1 = index1;
         this.index2 = index2;
         this.sizeMatch = sizeMatch;
         this.type = type;
+        this.modifier = modifier;
     }
 
     getIndexSwapGem() {
+        console.log(this,'getIndexSwapGem')
         return [this.index1, this.index2];
     }
 }
